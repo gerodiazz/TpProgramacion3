7699c673d2bb738d689038837ff13010535c39c1
Gero <gerojbro@gmail.com>
2024-06-18 19:38:39 -0300

    primer commit

---
+++ Domain/Base/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Domain.Base.Entity;
+
+namespace Domain.Base
+{
+    public class Entity
+    {
+        public abstract class Entity<TIdType> : IEntity<TIdType> where TIdType : IComparable
+        {
+            public abstract TIdType GetId();
+        }
+    }
+}
